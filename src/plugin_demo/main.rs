4db3ddcf2cf86ea8ed9520836309b67bf3510bf2
Gris Ge <fge@redhat.com>
2024-03-08 19:46:04 +0800

    Add demo external plugin
    Signed-off-by: Gris Ge <fge@redhat.com>

---
+++ src/plugin_demo/main.rs
@@ -0,0 +1,47 @@
+// SPDX-License-Identifier: Apache-2.0
+
+use std::sync::{Arc, Mutex};
+
+use nipart::{NipartError, NipartEvent, NipartExternalPlugin, NipartRole};
+use tokio::sync::mpsc::Sender;
+
+#[derive(Debug, Default)]
+struct NipartPluginDemoShareData {}
+
+impl NipartPluginDemoShareData {
+    fn _clear(&mut self) {}
+}
+
+#[derive(Debug)]
+struct NipartPluginDemo {
+    _data: Mutex<NipartPluginDemoShareData>,
+}
+
+impl NipartExternalPlugin for NipartPluginDemo {
+    const PLUGIN_NAME: &'static str = "demo";
+    const LOG_SUFFIX: &'static str = " (plugin demo)\n";
+
+    fn roles() -> Vec<NipartRole> {
+        vec![NipartRole::Monitor]
+    }
+
+    async fn init() -> Result<Self, NipartError> {
+        Ok(Self {
+            _data: Mutex::new(NipartPluginDemoShareData::default()),
+        })
+    }
+
+    async fn handle_event(
+        _plugin: &Arc<Self>,
+        _to_daemon: &Sender<NipartEvent>,
+        event: NipartEvent,
+    ) -> Result<(), NipartError> {
+        log::warn!("Plugin demo got unknown event {event:?}");
+        Ok(())
+    }
+}
+
+#[tokio::main]
+async fn main() -> Result<(), NipartError> {
+    NipartPluginDemo::run().await
+}
