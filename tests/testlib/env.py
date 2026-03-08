efb143b5f9bd5eccdb2e9be67bb3796cf08b156c
Gris Ge <fge@redhat.com>
2026-03-08 22:17:01 +0800

    test: Enable WIFI test on OS with mac80211_hwsim kernel module
    Any OS with `mac80211_hwsim` will run WIFI test.
    
    Unfortunately, github CI is using azure kernel which does not ship this
    `mac80211_hwsim` kernel module at any azure kernel sub-packages.
    
    We may use nested virtualization to test this in the future.
    
    Signed-off-by: Gris Ge <fge@redhat.com>

--- tests/testlib/env.py
+++ tests/testlib/env.py
@@ -2,12 +2,17 @@
 
 import os
 import pathlib
-
-
-def is_fedora():
-    return os.path.exists("/etc/fedora-release")
+from .cmdlib import exec_cmd
 
 
 def npt_path():
     project_dir = pathlib.Path(__file__).parent.parent.parent.resolve()
     return f"{project_dir}/target/debug/npt"
+
+
+def has_kernel_module(name):
+    try:
+        exec_cmd(f"modprobe {name} -n".split())
+        return True
+    except Exception:
+        return False
